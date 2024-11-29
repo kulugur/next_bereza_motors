0190eca2f43f5b9a039e3719a9d28785ea6e5e1f
Igor medvedev <medvede.kfx@gmail.com>
2024-11-29 14:09:11 +0400

    fix_main_page

--- app/page.js
+++ app/page.js
@@ -3,12 +3,12 @@ import Nav_brend from './components/Naw_brend'
 import Nav_bar from './components/Nav_bar'
 import Auto_box from'./components/Auto_box'
 
-async function fetcBrends(){
-  const brend = await fetch('http://127.0.0.1:8000/brands')
-  const res = await brend.json()
+// async function fetcBrends(){
+//   const brend = await fetch('http://127.0.0.1:8000/brands')
+//   const res = await brend.json()
 
-  return res
-}
+//   return res
+// }
 
 
 
